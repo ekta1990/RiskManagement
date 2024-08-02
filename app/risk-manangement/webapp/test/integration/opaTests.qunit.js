bb3284e3b6f31009568aa29e8ce2c1b0346d7107
ekta Raghav <eraghav1990@gmail.com>
2024-08-02 05:42:51 +0000

    Initial Commit

---
+++ app/risk-manangement/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'riskmanangement/test/integration/FirstJourney',
+		'riskmanangement/test/integration/pages/RisksList',
+		'riskmanangement/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('riskmanangement') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
---
+++ app/services.cds
@@ -0,0 +1,2 @@
+
+using from './risk-manangement/annotations';
